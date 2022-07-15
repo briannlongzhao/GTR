dc197a47ec1facae5f99bfd4c99d1eba654ab0e0
Brian Zhao <briannlongzhao@gmail.com>
2022-07-15 12:21:27 -0700

    add set TMPDIR in train_net.py

--- train_net.py
+++ train_net.py
@@ -275,7 +275,7 @@ if __name__ == "__main__":
         os.environ["TMPDIR"] = "/lab/tmpig8e/u/brian-data"
     elif re.search("[a-z]\d\d-\d\d", hostname):
         os.environ["TMPDIR"] = "/scratch1/briannlz"
-    print(f"$HOSTNAME={os.environ['HOSTNAME']}")
+    print(f"$HOSTNAME={hostname}")
     print(f"$TMPDIR={os.environ['TMPDIR']}")
 
     launch(
