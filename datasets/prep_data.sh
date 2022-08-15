e2ba89ec4695e10cf2c2fb2d8ea4fa8b1805a2ed
briannlz <briannlongzhao@gmail.com>
2022-08-15 12:10:30 -0700

    update prep_data.sh

--- datasets/prep_data.sh
+++ datasets/prep_data.sh
@@ -240,7 +240,7 @@ TAO() {
 
 # Set TMPDIR if on iLab or Discovery
 if [[ $HOSTNAME =~ iGpu || $HOSTNAME =~ iLab ]]; then
-	export TMPDIR=/lab/tmpig8b/u/brian-data
+	export TMPDIR=/lab/tmpig8e/u/brian-data
 elif [[ $HOSTNAME =~ "discovery" || $HOSTNAME =~ "hpc" || $HOSTNAME =~ [a-z][0-9][0-9]-[0-9][0-9] ]]; then
     export TMPDIR=/scratch1/briannlz
     module load gcc
@@ -272,6 +272,7 @@ if [[ -v TMPDIR ]]; then
     if [[ $# -eq 1 ]]; then
         echo "preparing $1 only"
         $1 $GTR_DIR_TMP
+        echo "Done preparing $1"
         exit
     fi
 	MOT17 $GTR_DIR_TMP
